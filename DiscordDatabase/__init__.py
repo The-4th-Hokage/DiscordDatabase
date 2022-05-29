016baab83036bbff1bd1ef577e4e448c50d9a935
Miku Chan <34207400+Dhruvacube@users.noreply.github.com>
2022-05-29 11:48:15 +0530

    added version

--- DiscordDatabase/__init__.py
+++ DiscordDatabase/__init__.py
@@ -1,1 +1,25 @@
+__title__ = 'DiscordDatabase'
+__author__ = 'Dhruva Shaw'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021-present Dhruvacube'
+__version__ = '0.1.4'
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+from typing import NamedTuple, Literal
+import logging
+
 from .DiscordDatabase import DiscordDatabase
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    releaselevel: Literal["alpha", "beta", "candidate", "final"]
+    serial: int
+
+
+version_info: VersionInfo = VersionInfo(major=0, minor=1, micro=4, releaselevel='final', serial=0)
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+del logging, NamedTuple, Literal, VersionInfo
